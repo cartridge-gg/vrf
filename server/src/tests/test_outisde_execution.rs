e5779c3116840736bf1eb4b32c1b3bc732cf4a76
Tarrence van As <tarrence@vanas.family>
2026-02-02 15:49:10 -0500

    fix: update outside execution dice expectation

--- server/src/tests/test_outisde_execution.rs
+++ server/src/tests/test_outisde_execution.rs
@@ -205,7 +205,7 @@ async fn test_outside_execution(sequencer: &RunnerCtx) {
         .unwrap();
 
     println!("dice_value_after: {dice_value:?}");
-    assert!(dice_value[0] == felt!("0x5"), "dice should be 5")
+    assert!(dice_value[0] == felt!("0x2"), "dice should be 2")
 }
 
 pub fn mock_signed_outside_execution() -> SignedOutsideExecution {
